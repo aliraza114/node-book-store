44458fe81e8518e8a75b1e732e368e19520206f7
aliraza114 <aliraza3569@gmail.com>
2020-09-08 09:15:55 +0500

    working with pug template engine

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-const path = require('path')
+const path = require('path');
+
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
-app.use('/admin', adminRoutes)
-app.use(shopRoutes)
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.sendStatus(404).sendfile(path.join(__dirname, 'views', '404.html'))
-})
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+});
 
-app.listen(3000); // create server and listen for route
+app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,15 @@
-const path = require('path')
-const express = require('express')
-const rootDir = require('../util/path')
+const path = require('path');
 
-const router = express.Router()
+const express = require('express');
 
-router.get('/',(req, res, next) => {
-    res.sendfile(path.join(rootDir, 'views', 'shop.html'))
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
 });
 
-module.exports = router
+module.exports = router;
