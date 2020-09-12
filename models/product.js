dff61db4021849302217dce49867c27cfeaa99e9
aliraza114 <aliraza3569@gmail.com>
2020-09-12 18:05:58 +0500

    added routes and controllers and formatting code

--- models/product.js
+++ models/product.js
@@ -1,15 +1,40 @@
-const products = []
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'products.json'
+);
+
+const getProductsFromFile = cb => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        } else {
+            cb(JSON.parse(fileContent));
+        }
+    });
+};
 
 module.exports = class Product {
-    constructor(title){
-        this.title = title
+    constructor(title, imageUrl, description, price) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
     }
 
-    saveProduct() {
-        products.push(this)
+    save() {
+        getProductsFromFile(products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), err => {
+                console.log(err);
+            });
+        });
     }
 
-    static getAllProducts() {
-        return this.products
+    static fetchAll(cb) {
+        getProductsFromFile(cb);
     }
-}
+};
