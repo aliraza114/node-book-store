ab1d749fa5aa34d372e42e7807aee40258dca4e4
aliraza114 <aliraza3569@gmail.com>
2020-10-05 10:28:47 +0500

    delete done

--- controllers/admin.js
+++ controllers/admin.js
@@ -88,7 +88,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
+  Product.findByIdAndRemove(prodId)
     .then(result => {
       console.log('Deleted product', result)
       res.redirect('/admin/products');
